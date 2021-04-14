f87b9533edbece0ad1936c78abcf3ba99bacc444
Justin Howell <justin@justinrhowell.com>
2021-04-13 22:17:46 -0400

    Add files via upload

---
+++ states-lived.js
@@ -0,0 +1,202 @@
+var statesLived = [
+  {
+    state: "Alabama",
+    visited: 0,
+  },
+  {
+    state: "Alaska",
+    visited: 100,
+  },
+  {
+    state: "Arkansas",
+    visited: 20,
+  },
+  {
+    state: "Arizona",
+    visited: 700,
+  },
+  {
+    state: "California",
+    visited: 999,
+  },
+  {
+    state: "Colorado",
+    visited: 600,
+  },
+  {
+    state: "Connecticut",
+    visited: 300,
+  },
+  {
+    state: "Delaware",
+    visited: 102,
+  },
+  {
+    state: "Florida",
+    visited: 850,
+  },
+  {
+    state: "Georgia",
+    visited: 0,
+  },
+  {
+    state: "Hawaii",
+    visited: 80,
+  },
+  {
+    state: "Iowa",
+    visited: 0,
+  },
+  {
+    state: "Idaho",
+    visited: 1,
+  },
+  {
+    state: "Illinois",
+    visited: 1,
+  },
+  {
+    state: "Indiana",
+    visited: 0,
+  },
+  {
+    state: "Kansas",
+    visited: 0,
+  },
+  {
+    state: "Kentucky",
+    visited: 998,
+  },
+  {
+    state: "Louisiana",
+    visited: 0,
+  },
+  {
+    state: "Maine",
+    visited: 1,
+  },
+  {
+    state: "Maryland",
+    visited: 1000,
+  },
+  {
+    state: "Massachusetts",
+    visited: 1,
+  },
+  {
+    state: "Michigan",
+    visited: 0,
+  },
+  {
+    state: "Minnesota",
+    visited: 1,
+  },
+  {
+    state: "Missouri",
+    visited: 0,
+  },
+  {
+    state: "Mississippi",
+    visited: 0,
+  },
+  {
+    state: "Montana",
+    visited: 1,
+  },
+  {
+    state: "North Carolina",
+    visited: 0,
+  },
+  {
+    state: "North Dakota",
+    visited: 0,
+  },
+  {
+    state: "Nebraska",
+    visited: 0,
+  },
+  {
+    state: "New Hampshire",
+    visited: 1,
+  },
+  {
+    state: "New Jersey",
+    visited: 0,
+  },
+  {
+    state: "New Mexico",
+    visited: 1,
+  },
+  {
+    state: "Nevada",
+    visited: 1,
+  },
+  {
+    state: "New York",
+    visited: 2,
+  },
+  {
+    state: "Ohio",
+    visited: 600,
+  },
+  {
+    state: "Oklahoma",
+    visited: 0,
+  },
+  {
+    state: "Oregon",
+    visited: 1,
+  },
+  {
+    state: "Pennsylvania",
+    visited: 1,
+  },
+  {
+    state: "Rhode Island",
+    visited: 1,
+  },
+  {
+    state: "South Carolina",
+    visited: 0,
+  },
+  {
+    state: "South Dakota",
+    visited: 0,
+  },
+  {
+    state: "Tennessee",
+    visited: 0,
+  },
+  {
+    state: "Texas",
+    visited: 400,
+  },
+  {
+    state: "Utah",
+    visited: 1,
+  },
+  {
+    state: "Virginia",
+    visited: 0,
+  },
+  {
+    state: "Vermont",
+    visited: 0,
+  },
+  {
+    state: "Washington",
+    visited: 1,
+  },
+  {
+    state: "Wisconsin",
+    visited: 0,
+  },
+  {
+    state: "West Virginia",
+    visited: 300,
+  },
+  {
+    state: "Wyoming",
+    visited: 1,
+  },
+];
---
+++ statesAscend.js
@@ -0,0 +1,202 @@
+var statesAscend = [
+  {
+    state: "Alabama",
+    visited: 100,
+  },
+  {
+    state: "Alaska",
+    visited: 50,
+  },
+  {
+    state: "Arkansas",
+    visited: 45,
+  },
+  {
+    state: "Arizona",
+    visited: 30,
+  },
+  {
+    state: "California",
+    visited: 76,
+  },
+  {
+    state: "Colorado",
+    visited: 78,
+  },
+  {
+    state: "Connecticut",
+    visited: 44,
+  },
+  {
+    state: "Delaware",
+    visited: 35,
+  },
+  {
+    state: "Florida",
+    visited: 45,
+  },
+  {
+    state: "Georgia",
+    visited: 76,
+  },
+  {
+    state: "Hawaii",
+    visited: 78,
+  },
+  {
+    state: "Iowa",
+    visited: 76,
+  },
+  {
+    state: "Idaho",
+    visited: 44,
+  },
+  {
+    state: "Illinois",
+    visited: 35,
+  },
+  {
+    state: "Indiana",
+    visited: 45,
+  },
+  {
+    state: "Kansas",
+    visited: 78,
+  },
+  {
+    state: "Kentucky",
+    visited: 44,
+  },
+  {
+    state: "Louisiana",
+    visited: 35,
+  },
+  {
+    state: "Maine",
+    visited: 45,
+  },
+  {
+    state: "Maryland",
+    visited: 76,
+  },
+  {
+    state: "Massachusetts",
+    visited: 78,
+  },
+  {
+    state: "Michigan",
+    visited: 44,
+  },
+  {
+    state: "Minnesota",
+    visited: 35,
+  },
+  {
+    state: "Missouri",
+    visited: 76,
+  },
+  {
+    state: "Mississippi",
+    visited: 45,
+  },
+  {
+    state: "Montana",
+    visited: 67,
+  },
+  {
+    state: "North Carolina",
+    visited: 35,
+  },
+  {
+    state: "North Dakota",
+    visited: 45,
+  },
+  {
+    state: "Nebraska",
+    visited: 44,
+  },
+  {
+    state: "New Hampshire",
+    visited: 45,
+  },
+  {
+    state: "New Jersey",
+    visited: 76,
+  },
+  {
+    state: "New Mexico",
+    visited: 78,
+  },
+  {
+    state: "Nevada",
+    visited: 35,
+  },
+  {
+    state: "New York",
+    visited: 44,
+  },
+  {
+    state: "Ohio",
+    visited: 76,
+  },
+  {
+    state: "Oklahoma",
+    visited: 78,
+  },
+  {
+    state: "Oregon",
+    visited: 44,
+  },
+  {
+    state: "Pennsylvania",
+    visited: 35,
+  },
+  {
+    state: "Rhode Island",
+    visited: 35,
+  },
+  {
+    state: "South Carolina",
+    visited: 45,
+  },
+  {
+    state: "South Dakota",
+    visited: 76,
+  },
+  {
+    state: "Tennessee",
+    visited: 78,
+  },
+  {
+    state: "Texas",
+    visited: 44,
+  },
+  {
+    state: "Utah",
+    visited: 45,
+  },
+  {
+    state: "Virginia",
+    visited: 78,
+  },
+  {
+    state: "Vermont",
+    visited: 76,
+  },
+  {
+    state: "Washington",
+    visited: 44,
+  },
+  {
+    state: "Wisconsin",
+    visited: 78,
+  },
+  {
+    state: "West Virginia",
+    visited: 35,
+  },
+  {
+    state: "Wyoming",
+    visited: 93,
+  },
+];
